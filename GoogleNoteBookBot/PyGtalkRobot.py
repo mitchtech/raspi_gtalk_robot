796e6f065554fd0661788be8ad63a4bc5e6d582f
ldmiao <ldmiao@042f34ec-b249-0410-8317-d9561670e1e7>
2008-04-11 17:42:08 +0000

    git-svn-id: http://pygtalkrobot.googlecode.com/svn/trunk/pygtalkrobot/src@42 042f34ec-b249-0410-8317-d9561670e1e7

--- GoogleNoteBookBot/PyGtalkRobot.py
+++ GoogleNoteBookBot/PyGtalkRobot.py
@@ -145,7 +145,7 @@ class GtalkRobot:
     # "debug" parameter specifies the debug IDs that will go into debug output.
     # You can either specifiy an "include" or "exclude" list. The latter is done via adding "always" pseudo-ID to the list.
     # Full list: ['nodebuilder', 'dispatcher', 'gen_auth', 'SASL_auth', 'bind', 'socket', 'CONNECTproxy', 'TLS', 'roster', 'browser', 'ibb'].
-    def __init__(self, server_host="gmail.com", server_port=5223, debug=[]):
+    def __init__(self, server_host="talk.google.com", server_port=5223, debug=[]):
         self.debug = debug
         self.server_host = server_host
         self.server_port = server_port
