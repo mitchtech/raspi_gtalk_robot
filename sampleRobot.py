1fc61c699dacdfeccaf23e806e198a9b0a8e8d58
ldmiao <ldmiao@042f34ec-b249-0410-8317-d9561670e1e7>
2008-04-18 07:21:37 +0000

    git-svn-id: http://pygtalkrobot.googlecode.com/svn/trunk/pygtalkrobot/src@61 042f34ec-b249-0410-8317-d9561670e1e7

--- sampleRobot.py
+++ sampleRobot.py
@@ -30,40 +30,50 @@ import time
 from PyGtalkRobot import GtalkRobot
 
 ############################################################################################################################
-class GNoteBot(GtalkRobot):
-    #Pattern Tips:
+
+class SampleBot(GtalkRobot):
+    
+    #Regular Expression Pattern Tips:
     # I or IGNORECASE <=> (?i)      case insensitive matching
     # L or LOCALE <=> (?L)          make \w, \W, \b, \B dependent on the current locale
     # M or MULTILINE <=> (?m)       matches every new line and not only start/end of the whole string
     # S or DOTALL <=> (?s)          '.' matches ALL chars, including newline
     # U or UNICODE <=> (?u)         Make \w, \W, \b, and \B dependent on the Unicode character properties database.
     # X or VERBOSE <=> (?x)         Ignores whitespace outside character sets
-
-    #command_ is the command prefix, 001 is the priviledge num, setState is the method name
+    
+    #"command_" is the command prefix, "001" is the priviledge num, "setState" is the method name.
+    #This method is used to change the state and status text of the bot.
     def command_001_setState(self, user, message, args):
-        """(available|online|on|busy|dnd|away|idle|out|off|xa)( +(.*))?$(?i)"""
+        #the __doc__ of the function is the Regular Expression of this command, if matched, this command method will be called. 
+        #The parameter "args" is a list, which will hold the matched string in parenthesis of Regular Expression.
+        '''(available|online|on|busy|dnd|away|idle|out|off|xa)( +(.*))?$(?i)'''
         show = args[0]
         status = args[1]
         jid = user.getStripped()
+
         # Verify if the user is the Administrator of this bot
         if jid == 'ldmiao@gmail.com':
             print jid, " ---> ",bot.getResources(jid), bot.getShow(jid), bot.getStatus(jid)
             self.setState(show, status)
-            self.replyMessage(user, "状态设置成功！")
+            self.replyMessage(user, "State settings changed！")
 
-    def command_002_justSave(self, user, message, args):
-        """(.*?\n.*)(?s)(?m)"""
-        #self.replyMessage(user, "\n"+message + "\n多行内容！")
-        self.replyMessage(user, "\n"+args[0] + "\n多行内容！")
-        self.replyMessage(user, time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
+    #This method is used to send email for users.
+    def command_002_SendEmail(self, user, message, args):
+        '''[email|mail|em|m]\s+(.*?@.+?)\s+(.*?),\s*(.*?)(?i)'''
+        email_addr = args[0]
+        subject = args[1]
+        body = args[2]
+        #call_send_email_function(email_addr, subject,  body)
+        
+        self.replyMessage(user, "\nEmail sent to "+ email_addr +" at: "+time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
     
+    #This method is used to response users.
     def command_100_default(self, user, message, args):
-        """.*?(?s)(?m)"""
+        '''.*?(?s)(?m)'''
         self.replyMessage(user, time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
 
 ############################################################################################################################
 if __name__ == "__main__":
-    bot = GNoteBot()
-    #bot = GNoteBot(debug=['always'])
+    bot = SampleBot()
     bot.setState('available', "Simple Gtalk Robot")
     bot.start("PyGtalkRobot@gmail.com", "PyGtalkRobotByLdmiao")
