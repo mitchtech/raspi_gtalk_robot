91f10cd9897781593de5ddf7a3ab8d83b525a8f2
ldmiao <ldmiao@042f34ec-b249-0410-8317-d9561670e1e7>
2008-04-18 05:47:05 +0000

    git-svn-id: http://pygtalkrobot.googlecode.com/svn/trunk/pygtalkrobot/src@57 042f34ec-b249-0410-8317-d9561670e1e7

--- AIAssistantBot/AIAssistantBot.py
+++ AIAssistantBot/AIAssistantBot.py
@@ -89,7 +89,11 @@ class AIABot(GtalkRobot):
             self.replyMessage(user, "状态设置成功！")
     def command_002_saveToAIAWeb(self, user, message, args):
         """[h|help|how](?i)"""
-        self.replyMessage(user, "All Chats will be Saved in http://aiassistant.appspot.com/chat/")
+        helpMsg = "Artifical Intelligence Assistant (AIA)\n"
+        helpMsg = helpMsg + "AIA is an assistant that can do tasks like keeping accounts, arranging TODOs and so on intelligently.\n"
+        helpMsg = helpMsg + "All Chats will be Saved in http://aiassistant.appspot.com/chat/"
+        
+        self.replyMessage(user, helpMsg)
         
     def command_003_saveToAIAWeb(self, user, message, args):
         """(.*)(?s)(?m)"""
